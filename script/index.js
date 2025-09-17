24c37c6fe16a1961b89b540c54a0045a5212749a
iivanov-dev <1999.i.ivanov@mail.ru>
2025-09-17 14:19:21 +0500

    part 3.8

---
+++ script/index.js
@@ -0,0 +1,19 @@
+const modal = document.querySelector('.cart-modal__overlay')
+const cartBtn = document.querySelector('#cart-button')
+const closeBtn = document.querySelector('#closeBtn')
+
+const openModal = () => {
+    modal.classList.add('open')
+}
+
+const closeModal = () => {
+    modal.classList.remove('open')
+}
+
+cartBtn.addEventListener('click', () => {
+    openModal()
+})
+
+closeBtn.addEventListener('click', () => {
+    closeModal()
+})
