6f4fa28d6cd1fbcfa0fb1cddeba1a921a4da0051
iivanov-dev <1999.i.ivanov@mail.ru>
2025-09-17 15:47:41 +0500

    part 4.2

--- script/index.js
+++ script/index.js
@@ -1,22 +1,146 @@
-const modal = document.querySelector('.cart-modal__overlay')
-const cartBtn = document.querySelector('#cart-button')
+const modalFunc = () => {
+  const modal = document.querySelector(".cart-modal__overlay");
+  const cartBtn = document.querySelector("#cart-button");
 
-const openModal = () => {
-    modal.classList.add('open')
-}
+  const openModal = () => {
+    modal.classList.add("open");
+  };
 
-const closeModal = () => {
-    modal.classList.remove('open')
-}
+  const closeModal = () => {
+    modal.classList.remove("open");
+  };
 
-cartBtn.addEventListener('click', () => {
-    openModal()
-})
+  cartBtn.addEventListener("click", () => {
+    openModal();
+  });
 
-modal.addEventListener('click', (event) => {   
-    
-    if (event.target.classList.contains('cart-modal__overlay') || event.target.closest('.cart-modal__header--close')) {
-        closeModal()
+  modal.addEventListener("click", (event) => {
+    if (
+      event.target.classList.contains("cart-modal__overlay") ||
+      event.target.closest(".cart-modal__header--close")
+    ) {
+      closeModal();
     }
-    
-})
+  });
+};
+
+const restsFunc = () => {
+  const container = document.querySelector("#rests-container");
+
+  const restArray = [
+    {
+      id: 0,
+      title: "Пицца плюс",
+      time: 50,
+      rating: 4.5,
+      price: 900,
+      type: "Пицца",
+      image: "rest-1.jpg"
+    },
+    {
+      id: 1,
+      title: "Тануки",
+      time: 50,
+      rating: 4.5,
+      price: 900,
+      type: "Пицца",
+      image: "rest-2.jpg"
+    },
+    {
+      id: 2,
+      title: "FoodBand",
+      time: 45,
+      rating: 4.7,
+      price: 850,
+      type: "Пицца",
+      image: "rest-3.jpg"
+    },
+    {
+      id: 3,
+      title: "Жадина-пицца",
+      time: 60,
+      rating: 4.3,
+      price: 700,
+      type: "Пицца",
+      image: "rest-4.jpg"
+    },
+    {
+      id: 4,
+      title: "Точка еды",
+      time: 40,
+      rating: 4.6,
+      price: 950,
+      type: "Пицца",
+      image: "rest-5.jpg"
+    },
+    {
+      id: 5,
+      title: "PizzaBurger",
+      time: 55,
+      rating: 4.4,
+      price: 1000,
+      type: "Пицца",
+      image: "rest-6.jpg"
+    }
+  ];
+
+  console.log(container);
+
+  const loading = () => {
+    container.innerHTML =
+      '<p style = "width: 100%; text-align: center;"> Load ... </p>';
+  };
+
+  const renderRests = (array) => {
+    container.innerHTML = "";
+
+    array.forEach((card) => {
+      container.insertAdjacentHTML(
+        "beforeend",
+        `
+            <div class="products-card">
+                <a href="./goods.html?id=${card.id}" class="products-card__image">
+                  <img src="./images/rests/${card.image}" alt="${card.image}" />
+                </a>
+
+                <div class="products-card__description">
+                  <div class="products-card__description-row">
+                    <h4 class="products-card__description--title">
+                        ${card.title}
+                    </h4>
+                    <div class="products-card__description--badge">
+                        ${card.time} мин</div>
+                  </div>
+
+                  <div class="products-card__description-row">
+                    <div class="products-card__description-info">
+                      <div class="products-card__description-info--raiting">
+                        <img src="./images/icons/star.svg" alt="star" />
+                        ${card.rating}
+                      </div>
+                      <div class="products-card__description-info--price">
+                        От ${card.price} ₽
+                      </div>
+                      <div class="products-card__description-info--group">
+                        ${card.type}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            `
+      );
+    });
+  };
+
+  if (container){
+    loading();
+
+    setTimeout(() => {
+      renderRests(restArray);
+    }, 1000);
+  }
+};
+
+modalFunc();
+restsFunc();
